f549cd937146e8223b461a7481456529ef8c9f45
Nicholas <nskg@pm.me>
2023-05-25 13:06:09 +0200

    cascade simulation configs

---
+++ acg/baseconfiguration/cascade_sim_configuration.py
@@ -0,0 +1,155 @@
+## In this file we define the actors and the default values of them
+# Based on the homemade modules:
+from acg.configgenerator.agent_configuration import AgentConfiguration
+
+### MARKET MAKERS ###
+marketmaker_traders = AgentConfiguration( "MarketMaker",
+                                          {"initialCash": 1000000,
+                                           "minLatency": 25,
+                                           "meanLatency": 150,
+                                           "latencyStdevPct": 0.5,
+                                           "symbolA": "ABC",
+                                           "symbolACorrelation": 0.02,
+                                           "symbolB": "DEF",
+                                           "symbolBCorrelation": 0.02,
+                                           "symbolC": "GHI",
+                                           "symbolCCorrelation": 0.02,
+                                           "updateSecs": [600, 601, True],
+                                           "lookbackPeriods": 50,
+                                           "mpLookback": [120,121, True],
+                                           "spreadFactor": [0.15, 0.16, True],
+                                           "recalcOnLoss": "true",
+                                           "workSize": 105,
+                                           "levels": 30,
+                                           "minSpread": 0.0101,
+                                           "inventoryPenalty": 0.0,
+                                           "inventoryFactor" : 0.000333333,
+                                           "followTrend" : 0.0,
+                                           "scaleWorkSize": "false"}
+                                          )
+
+
+
+### INTRADAY TRADERS ###
+
+# Shared variables
+intraday_traders_minLatency = 100
+intraday_traders_meanLatency = 500000
+intraday_traders_latencyStdevPct = 0.5
+intraday_traders_agentSymbols = "ABC"
+
+
+## Aggressor traders
+aggressor_traders_ST = AgentConfiguration( "AggressorTrendST", {
+    "initialCash" : 300000.0,
+    "minLatency" : intraday_traders_minLatency,
+    "meanLatency" : intraday_traders_meanLatency,
+    "latencyStdevPct" : intraday_traders_latencyStdevPct,
+    "agentSymbols" : intraday_traders_agentSymbols,
+    "lookback" : [10, 20],
+    "triggerSecs" : [5*60, 120*60],
+    "stopMultiplier" : [1.5, 3.5, True],
+    "parameter" : [1.75, 3.0, True],
+    "recalcOnLoss" : "false",
+    "getFlatOnClose": "false",
+    "ziReversionFactor" : 0}
+                                           )
+
+
+## BreakoutTrend traders
+breakout_traders_ST = AgentConfiguration( "BreakoutTrendST", {
+    "initialCash": 300000.0,
+    "minLatency": intraday_traders_minLatency,
+    "meanLatency": intraday_traders_meanLatency,
+    "latencyStdevPct": intraday_traders_latencyStdevPct,
+    "agentSymbols": intraday_traders_agentSymbols,
+    "lookback": [10, 20],
+    "triggerSecs": [5 * 60, 60 * 60],
+    "stopMultiplier" : [1.5, 3.5, True],
+    "parameter": [1., 3., True],
+    "recalcOnLoss": "false","getFlatOnClose": "false",
+    "ziReversionFactor" : 0}
+                                          )
+
+
+## RsiReversion
+rsireversion_traders_ST = AgentConfiguration( "RsiReversionST", {
+    "initialCash": 300000.0,
+    "minLatency": intraday_traders_minLatency,
+    "meanLatency": intraday_traders_meanLatency,
+    "latencyStdevPct": intraday_traders_latencyStdevPct,
+    "agentSymbols": intraday_traders_agentSymbols,
+    "lookback": [10, 20],
+    "triggerSecs": [30 * 60, 120 * 60],
+    "stopMultiplier" : [1.5, 3.5, True],
+    "parameter": [5.0, 25.0, True],
+    "recalcOnLoss": "false",
+    "getFlatOnClose": "false",
+    "ziReversionFactor" : 0}
+                                              )
+
+
+
+## PullbackReversion traders
+pullbackreversion_traders_ST = AgentConfiguration( "PullbackReversionST", {
+    "initialCash": 300000,
+    "minLatency": intraday_traders_minLatency,
+    "meanLatency": intraday_traders_meanLatency,
+    "latencyStdevPct": intraday_traders_latencyStdevPct,
+    "agentSymbols": intraday_traders_agentSymbols,
+    "lookback": [10, 20],
+    "triggerSecs": [15 * 60, 180 * 60],
+    "stopMultiplier" : [1.5, 3.5, True],
+    "parameter": [0.0, 0.0, True],
+    "recalcOnLoss": "false",
+    "getFlatOnClose": "false",
+    "ziReversionFactor" : 0}
+                                                   )
+
+
+
+## ScalperReversion traders
+scalperreversion_traders_ST = AgentConfiguration( "ScalperReversionST", {
+    "initialCash": 300000,
+    "minLatency": intraday_traders_minLatency,
+    "meanLatency": intraday_traders_meanLatency,
+    "latencyStdevPct": intraday_traders_latencyStdevPct,
+    "agentSymbols": intraday_traders_agentSymbols,
+    "lookback": [25, 50],
+    "triggerSecs": [30 * 60, 90 * 60],
+    "stopMultiplier" : [1.5, 3.5, True],
+    "parameter": [1.25, 2.5, True],
+    "recalcOnLoss": "false",
+    "getFlatOnClose": "false",
+    "ziReversionFactor" : 0}
+                                                  )
+
+
+zero_info_trader_ST = AgentConfiguration( "ZeroInfoST", {
+    "initialCash": 200000,
+    "minLatency": intraday_traders_minLatency,
+    "meanLatency": intraday_traders_meanLatency,
+    "latencyStdevPct": intraday_traders_latencyStdevPct,
+    "agentSymbols": intraday_traders_agentSymbols,
+    "lookback": [20, 50],
+    "triggerSecs": [600, 2700],
+    "stopMultiplier": [10., 20., True],
+    "parameter": [200, 900, True],
+    "recalcOnLoss": "false",
+    "getFlatOnClose" : "false",
+    "ziReversionFactor" : 0.5}
+                                          )
+
+
+portfolio_trader = AgentConfiguration( "PortfolioTrader", {
+    "initialCash": 10000000,
+    "minLatency": 100,
+    "meanLatency": 60000000,
+    "latencyStdevPct": 0.5,
+    "agentSymbols": intraday_traders_agentSymbols,
+    "portfolioHorizonRangeMin" : 5,
+    "portfolioHorizonRangeMax" : 60,
+    "initialVol" : 0.02,
+    "epsGrowthRate": 0.0003,
+    "epsGrowthRateStd": 0.0006}
+                                          )
---
+++ acg/simulations/make_cascade_sim_configs.py
@@ -0,0 +1,131 @@
+import sys
+sys.path.append('../..')
+
+from acg.configgenerator import configuration_generator
+from acg.baseconfiguration.cascade_sim_configuration import *
+import pandas as pd
+import argparse
+import datetime
+from pandas.tseries.offsets import BDay
+import numpy as np
+import sys
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument("--out-dir", dest="conf_dir", default = "",type = str, help = "output directory")
+parser.add_argument("--name", dest="run_name", type = str, help= "name of simulation in ammps", required=True)
+parser.add_argument("--seed", dest="seed", type = int, help = "random seed",required=True)
+parser.add_argument("--days", dest="number_of_days", type = int, default = 261, help = "days to simulate")
+parser.add_argument("--zi-scaler", dest = "ZI_n_scaler", type = float, default=1.0, help = "scaling factor scaling the number of Zi agnets",required=True )
+parser.add_argument("--algo-cash-scaler", dest = "algo_cash_scaler", type = float, default=1.0, help = "scaling factor scaling cash for algo/intraday agents",required=True )
+
+
+if __name__ == "__main__":
+
+    args = parser.parse_args()
+    args_string = ""
+    for i in range(1,len(sys.argv)):
+        args_string = args_string + " " + sys.argv[i]
+    print("Started with the following arguments:", args_string)
+    number_of_days = args.number_of_days
+    config_name = "get from somewhere"
+    np.random.seed(args.seed)
+
+    new_config = configuration_generator.ConfigurationWriter(
+        config_name=args.run_name,
+        seed=args.seed,
+        path=args.conf_dir,
+        file_name=args.run_name # We may want a different file name than run name
+    )
+    start_date = "1/1/2017 9:30:00"
+    start_date_dt = datetime.datetime.strptime(start_date, "%m/%d/%Y %H:%M:%S")
+    end_date_dt = start_date_dt + BDay(number_of_days)
+    # start and end dates year, month, day, hour, minute,second
+    new_config.add_dates(
+        start_date_dt.year, start_date_dt.month, start_date_dt.day, 9, 30, 00,
+        end_date_dt.year, end_date_dt.month, end_date_dt.day, 16, 30, 00
+    )
+
+    name = "MarketMaker"
+    new_config.add_agent(
+        name=name,
+        values=marketmaker_traders.make_param(np,9).to_dict(orient='records')
+    )
+
+    name = "PortfolioTrader"
+
+    new_config.add_agent(
+        name=name,
+        values=portfolio_trader.make_param(np,100).to_dict(orient='records')
+    )
+
+    # Zero info traders
+    name = "ZeroInfo"
+    n_zi_st = int(100 * args.ZI_n_scaler)
+    zi_st = zero_info_trader_ST.make_param(np,n=n_zi_st).to_dict(orient='records')
+
+    agent_values = zi_st
+
+    for d in agent_values:
+        d["parameter"] = d["triggerSecs"]
+
+    new_config.add_agent(
+        name=name,
+        values=agent_values
+    )
+
+    name = "AggressorTrend"
+    aggressor_traders_ST.parameters["initialCash"] = 300000.0 * args.algo_cash_scaler
+
+
+
+    new_config.add_agent(
+        name=name,
+        values=pd.concat([
+            aggressor_traders_ST.make_param(np,n=100)
+        ]).to_dict(orient='records')
+    )
+
+    name = "BreakoutTrend"
+    breakout_traders_ST.parameters["initialCash"] = 300000.0 * args.algo_cash_scaler
+
+
+    new_config.add_agent(
+        name=name,
+        values=pd.concat([
+            breakout_traders_ST.make_param(np,n=100)
+        ]).to_dict(orient='records')
+    )
+
+    name = "RsiReversion"
+    rsireversion_traders_ST.parameters["initialCash"] = 300000.0 * args.algo_cash_scaler
+
+    new_config.add_agent(
+        name=name,
+        values=pd.concat([
+            rsireversion_traders_ST.make_param(np,n=100)
+        ]).to_dict(orient='records')
+    )
+
+    name = "PullbackReversion"
+    pullbackreversion_traders_ST.parameters["initialCash"] = 300000.0 * args.algo_cash_scaler
+
+    new_config.add_agent(
+        name=name,
+        values=pd.concat([
+            pullbackreversion_traders_ST.make_param(np,n=100)
+        ]).to_dict(orient='records')
+    )
+
+    name = "ScalperReversion"
+    scalperreversion_traders_ST.parameters["initialCash"] = 300000.0 * args.algo_cash_scaler
+
+    new_config.add_agent(
+        name=name,
+        values=pd.concat([
+            scalperreversion_traders_ST.make_param(np,n=100)
+        ]).to_dict(orient='records')
+    )
+
+    new_config.write_file()
+
+
