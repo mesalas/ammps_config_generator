32ad7d53a4777a5240ac0ec23b5af3cf59ba7489
Nicholas <nskg@pm.me>
2023-06-15 16:46:20 +0200

    market maker test

---
+++ acg/simulations/make_market_maker_test_config.py
@@ -0,0 +1,109 @@
+import sys
+sys.path.append('../..')
+
+from acg.configgenerator import configuration_generator
+from acg.baseconfiguration.basic_trader_test_configuration import *
+
+import pandas as pd
+import argparse
+import datetime
+from pandas.tseries.offsets import BDay
+import numpy as np
+import sys
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument("--out-dir", dest="conf_dir", default = "",type = str, help = "output directory")
+parser.add_argument("--name", dest="run_name", type = str, help= "name of simulation in ammps", required=True)
+parser.add_argument("--seed", dest="seed", type = int, help = "random seed",required=True)
+parser.add_argument("--market-maker-lookback-length", dest="market_maker_lookback_length", type = int, help = "",required=True)
+parser.add_argument("--market-maker-lookback-secs", dest="market_maker_lookback_secs", type = int, help = "",required=True)
+parser.add_argument("--market-maker-mp-lookback-length", dest="market_maker_mp_lookback_length", type = int, help = "",required=True)
+parser.add_argument("--n-day-traders", dest="n_day_traders", type = int, help = "",required=True)
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    args_string = ""
+    for i in range(1,len(sys.argv)):
+        args_string = args_string + " " + sys.argv[i]
+    print("Started with the following arguments:", args_string)
+
+    np.random.seed(args.seed)
+
+    new_config = configuration_generator.ConfigurationWriter(
+        config_name=args.run_name,
+        seed=args.seed,
+        path=args.conf_dir,
+        file_name=args.run_name # We may want a different file name than run name
+    )
+    start_date = "1/1/2017 9:30:00"
+    start_date_dt = datetime.datetime.strptime(start_date, "%m/%d/%Y %H:%M:%S")
+    end_date_dt = start_date_dt + BDay(260)
+    # start and end dates year, month, day, hour, minute,second
+    new_config.add_dates(
+        start_date_dt.year, start_date_dt.month, start_date_dt.day, 9, 30, 00,
+        end_date_dt.year, end_date_dt.month, end_date_dt.day, 16, 30, 00
+    )
+
+    new_config.add_agent(
+        name=marketmaker_traders.name,
+        values=marketmaker_traders.make_param(np,3).to_dict(orient='records')
+    )
+
+    # Basic traders
+    name = "BasicTrader"
+
+    basic_traders = basic_trader.make_param(np,n=250).to_dict(orient='records')
+
+
+    agent_values = basic_traders
+    new_config.add_agent(
+        name=name,
+        values=agent_values
+    )
+
+    name = "AggressorTrend"
+    aggressor_traders_ST.parameters["initialCash"] = 100000.0
+
+
+
+    new_config.add_agent(
+        name=name,
+        values=pd.concat([
+            aggressor_traders_ST.make_param(np,n=args.n_day_traders)
+        ]).to_dict(orient='records')
+    )
+
+    name = "BreakoutTrend"
+    breakout_traders_ST.parameters["initialCash"] = 100000.0
+
+
+    new_config.add_agent(
+        name=name,
+        values=pd.concat([
+            breakout_traders_ST.make_param(np,n=args.n_day_traders)
+        ]).to_dict(orient='records')
+    )
+
+    name = "RsiReversion"
+    rsireversion_traders_ST.parameters["initialCash"] = 100000.0
+
+    new_config.add_agent(
+        name=name,
+        values=pd.concat([
+            rsireversion_traders_ST.make_param(np,n=args.n_day_traders)
+        ]).to_dict(orient='records')
+    )
+
+    name = "ScalperReversion"
+    scalperreversion_traders_ST.parameters["initialCash"] = 100000.0
+
+    new_config.add_agent(
+        name=name,
+        values=pd.concat([
+            scalperreversion_traders_ST.make_param(np,n=args.n_day_traders)
+        ]).to_dict(orient='records')
+    )
+
+    new_config.write_file()
+
+
